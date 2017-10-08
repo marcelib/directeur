77daa95dc1f19fe23e727cc10bcf03ce625e661e
Marceli Baczewski <marceli.baczewski@gmail.com>
2017-10-09 00:56:10 +0200

    Update readme + minor changes

--- src/main/java/com/braintri/directeur/data/Employee.java
+++ src/main/java/com/braintri/directeur/data/Employee.java
@@ -2,11 +2,21 @@ package com.braintri.directeur.data;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Data
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @ManyToOne
+    @JoinColumn(name="positionId")
+    public Position position;
+
+    public String name;
+    public String surname;
+    public String email;
 }
--- src/main/java/com/braintri/directeur/data/Position.java
+++ src/main/java/com/braintri/directeur/data/Position.java
@@ -1,4 +1,19 @@
 package com.braintri.directeur.data;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Position {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String positionName;
 }
